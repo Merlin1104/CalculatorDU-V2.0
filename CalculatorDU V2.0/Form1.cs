d861193f8129d5ee3c7e6dd630a9fc419da38d60
N <N@DESKTOP-1ITEO9G>
2018-02-21 20:18:38 +0100

    Kalkulacka

--- CalculatorDU V2.0/Form1.cs
+++ CalculatorDU V2.0/Form1.cs
@@ -16,5 +16,65 @@ namespace CalculatorDU_V2._0
         {
             InitializeComponent();
         }
+
+        double value = 0;
+        string operation = "";
+        bool operation_pressed = false;
+
+        private void button6_Click(object sender, EventArgs e) // C
+        {
+            result.Clear();
+            value = 0;
+        }
+
+        private void button_Click(object sender, EventArgs e) //cisla
+        {
+            if ((result.Text == "0")||(operation_pressed)) //ked he operation pressed true tak clearuje
+            {
+                result.Clear();
+            }
+
+            operation_pressed = false;
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e) //CE
+        {
+            result.Text = "0";
+        }
+
+        private void operator_click(object sender, EventArgs e) //operatory
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = Double.Parse(result.Text);
+            operation_pressed = true;
+            equation.Text = value + " " + operation;
+        }
+
+        private void button11_Click(object sender, EventArgs e) // =
+        {
+            equation.Text = "";
+            switch (operation)
+            {
+                
+                case "+":
+                    result.Text = (value + Double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * Double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / Double.Parse(result.Text)).ToString();
+                    break;
+                case "-":
+                    result.Text = (value - Double.Parse(result.Text)).ToString();
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
